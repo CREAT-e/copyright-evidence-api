6391b68b40499c7690379f0e1c54371cc602ee3f
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-06-16 22:36:15 +0100

    Started writing code to load studies from database and parse them into internal model.

--- src/server.py
+++ src/server.py
@@ -1,1 +1,18 @@
+from study_loader import StudyLoader
+from flask import Flask
+from pprint import pprint
+app = Flask(__name__)
 
+@app.route("/studies")
+def studies():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    loader = StudyLoader()
+
+    all_studies = loader.get_all_studies()
+
+    for study in all_studies:
+        pprint(study.authors)
+
+    app.run()
---
+++ src/study_parser.py
@@ -0,0 +1,47 @@
+from study import Study
+
+class StudyParser(object):
+
+    def parse_list(self,value):
+        return value.split(";")
+
+    def parse_author(self, study, value):
+        study.authors = self.parse_list(value)
+
+    def parse_title(self, study, value):
+        study.title = value
+
+    def parse_year(self, study, value):
+        study.year = value
+
+    def __init__(self):
+        self.handlers = {}
+        self.register('author', self.parse_author)
+        self.register('title', self.parse_title)
+        self.register('year', self.parse_year)
+
+    def register(self, property, handler):
+        self.handlers[property] = handler
+
+    def parse_prop(self, study, key, value):
+        key = key.replace("|", "").lower()
+
+        handler = self.handlers.get(key)
+        if (handler):
+            handler(study, value)
+
+
+    def parse_study(self,studyText):
+        lines = studyText.split('\n')
+        property_lines = filter(lambda line: line.startswith("|"), lines)
+
+        properties = map(lambda line: line.split("="), property_lines)
+
+        study = Study()
+
+        for prop in properties:
+            key = prop[0]
+            val = prop[1]
+            self.parse_prop(study, key, val)
+
+        return study
