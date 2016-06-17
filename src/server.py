0d5d177152a1a29d81ed2c145acfe60bf4862d20
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-06-17 09:24:39 +0100

    Work in progress: Return all studies from API.

--- src/server.py
+++ src/server.py
@@ -1,18 +1,31 @@
-from study_loader import StudyLoader
-from flask import Flask
+from pprint import PrettyPrinter
+import json
 from pprint import pprint
+from flask import Flask, jsonify
+from study_loader import StudyLoader
+
 app = Flask(__name__)
+app.config.from_envvar('CREATE_CFG')
+
+all_studies = []
 
 @app.route("/studies")
 def studies():
-    return "Hello World!"
+    results = map(lambda study: json.dumps(study.__dict__), all_studies)
+
+    payload = {
+        "results" : results
+    }
+
+    return json.dumps(payload)
 
 if __name__ == "__main__":
     loader = StudyLoader()
 
     all_studies = loader.get_all_studies()
+    pp = PrettyPrinter()
 
     for study in all_studies:
-        pprint(study.authors)
+        pp.pprint(study.authors)
 
     app.run()
--- src/study.py
+++ src/study.py
@@ -4,8 +4,7 @@ class Study(object):
     A model of all the metadata associated with a study on the wiki
     """
     def __init__(self, properties):
-
-                 self.name = properties.get('name')
-                 self.authors = properties.get('authors')
-                 self.title = properties.get('title')
-                 self.year = properties.get('year')
+         self.name = properties.get('name')
+         self.authors = properties.get('authors')
+         self.title = properties.get('title')
+         self.year = properties.get('year')
