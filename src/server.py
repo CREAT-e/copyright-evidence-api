9e51c4721707d550b6e6884c7db780fecf4bed9e
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-06-18 00:03:12 +0100

    parse some more paper properties

--- src/server.py
+++ src/server.py
@@ -1,6 +1,4 @@
-from pprint import PrettyPrinter
 import json
-from pprint import pprint
 from flask import Flask, jsonify
 from study_loader import StudyLoader
 
@@ -22,9 +20,5 @@ if __name__ == "__main__":
     loader = StudyLoader()
 
     all_studies = loader.get_all_studies()
-    pp = PrettyPrinter()
-
-    for study in all_studies:
-        pp.pprint(study.get('authors'))
 
     app.run()
--- src/study_parser.py
+++ src/study_parser.py
@@ -1,19 +1,39 @@
 class StudyParser(object):
 
-    def parse_list_strategy(self,value):
-        return value.split(";")
+    def strip_start_and_end(self,val):
+        return val.lstrip().rstrip()
+
+    def list_strategy(self,value):
+        vals = value.split(";")
+        return list(map(self.strip_start_and_end, vals))
 
     def single_field_strategy(self, value):
-        return value
+        return self.strip_start_and_end(value)
+
+    def bullet_points_strategy(self, value):
+        #print(value)
+        bullets = value.split("*")
+        points = list(map(self.strip_start_and_end, bullets))
+        return points
 
     def parse_using_strategy(self, study, give_key, parse_strategy, value):
         study[give_key] = parse_strategy(value)
 
     def __init__(self):
         self.handlers = {}
-        self.register('author', lambda study, val: self.parse_using_strategy(study, 'authors', self.parse_list_strategy, val))
+        self.register('author', lambda study, val: self.parse_using_strategy(study, 'authors', self.list_strategy, val))
         self.register('title', lambda study, val: self.parse_using_strategy(study, 'title', self.single_field_strategy, val))
-        self.register('year', lambda study, val: self.parse_using_strategy(study, 'year', self.parse_list_strategy, val))
+        self.register('year', lambda study, val: self.parse_using_strategy(study, 'year', self.single_field_strategy, val))
+        self.register('full citation', lambda study, val: self.parse_using_strategy(study, 'citation', self.single_field_strategy, val))
+        self.register('abstract', lambda study, val: self.parse_using_strategy(study, 'abstract', self.single_field_strategy, val))
+        self.register('link', lambda study, val: self.parse_using_strategy(study, 'link', self.single_field_strategy, val))
+        self.register('authentic link', lambda study, val: self.parse_using_strategy(study, 'open_access_link', self.single_field_strategy, val))
+        self.register('reference', lambda study, val: self.parse_using_strategy(study, 'reference', self.single_field_strategy, val))
+        self.register('plain text proposition', lambda study, val: self.parse_using_strategy(study, 'study_findings', self.bullet_points_strategy, val))
+
+        # TODO: These two need a different parser from the 'list_strategy' parser... (A. <value>, B.value)
+        self.register('fundamentalissue',lambda study, val: self.parse_using_strategy(study, 'relevant_issues', self.list_strategy, val))
+        self.register('evidencebasedpolicy',lambda study, val: self.parse_using_strategy(study, 'relevant_issues', self.list_strategy, val))
 
     def register(self, property, handler):
         self.handlers[property] = handler
